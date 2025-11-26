35db9f014808b75e350a9945a56c5e3287641504
mr <mr.scrpt@gmail.com>
2025-11-26 11:27:58 +0200

    refactor: name

--- src/presentation/web/react/src/shared/ui/button/model/useInputClsBuilder.modle.ts
+++ src/presentation/web/react/src/shared/ui/button/model/useInputClsBuilder.modle.ts
@@ -1,14 +1,6 @@
-// import { cvax } from "@/shared/lib/cvax";
 import type { ButtonSizeType } from "../domain/size.type";
 import type { ButtonStateType } from "../domain/state.type";
 import type { ButtonViewType } from "../domain/view.type";
-// import { useComputedInteractionClass } from "./useCompoundInteractionClass.model";
-// import { useCompoundStateClass } from "./useCompoundState.model";
-// import { buttonBaseCls, buttonNativeStateCls } from "../data/base.cls";
-// import { buttonViewCls } from "../data/view.cls";
-// import { buttonSizeCls } from "../data/size.cln";
-// import { buttonStateCls } from "../data/state/state.cls";
-// import { buttonInteractionCls } from "../data/interaction/interaction.cln";
 import { cn } from "@/shared/lib/shadcn";
 import { useButtonViewCls } from "./useInputViewCls.model";
 import { useButtonSizeCls } from "./useInputSizeCls.model";
@@ -26,9 +18,6 @@ type UseButtonBuilderParams = {
 export const useButtonClassBuilder = (params: UseButtonBuilderParams) => {
   const { view, size, state, className } = params;
 
-  // const compoundState = useCompoundStateClass({ view, state });
-  // const compoundInteraction = useComputedInteractionClass({ view, state });
-
   return {
     clsButton: cn(
       [
@@ -42,22 +31,4 @@ export const useButtonClassBuilder = (params: UseButtonBuilderParams) => {
       className,
     ),
   };
-
-  // const classes = cvax([...buttonBaseCls, ...buttonNativeStateCls], {
-  //   variants: {
-  //     view: buttonViewCls,
-  //     size: buttonSizeCls,
-  //     state: buttonStateCls,
-  //   },
-  //   multiVariants: {
-  //     interaction: buttonInteractionCls,
-  //   },
-  // })({
-  //   view,
-  //   size,
-  //   state: compoundState,
-  //   interaction: compoundInteraction,
-  // });
-
-  // return cn(classes, className);
 };
